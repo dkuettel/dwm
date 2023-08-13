359e8b4d02b0c2ec89e89a730895e845187d7fb1
Daniel Kuettel <dkuettel@gmail.com>
2023-08-13 23:56:16 +0200

    add tagnames to align with kmonad config

--- config.def.h
+++ config.def.h
@@ -20,7 +20,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1a", "2s", "3d", "4f", "5g", "6z", "7x", "8c", "9v" };
 
 static const Rule rules[] = {
 	/* xprop(1):
