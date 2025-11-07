bddbf9065ce2a2b881a300ea283ce94f44a32116
Daniel Kuettel <dkuettel@gmail.com>
2025-11-07 23:29:55 +0100

    systems

--- flake.nix
+++ flake.nix
@@ -3,31 +3,36 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, flake-utils }:
     let
-      pkgs = import nixpkgs { system = "x86_64-linux"; };
-      pkg = pkgs.stdenv.mkDerivation rec {
-        pname = "dwm";
-        version = "dk";
+      make = system:
+        let
+          pkgs = import nixpkgs { system = system; };
+          pkg = pkgs.stdenv.mkDerivation rec {
+            pname = "dwm";
+            version = "dk";
 
-        src = ./.;
+            src = ./.;
 
-        buildInputs = with pkgs; [
-          xorg.libX11
-          xorg.libXinerama
-          xorg.libXft
-        ];
+            buildInputs = with pkgs; [
+              xorg.libX11
+              xorg.libXinerama
+              xorg.libXft
+            ];
 
-        prePatch = ''
-          sed -i "s@/usr/local@$out@" config.mk
-        '';
+            prePatch = ''
+              sed -i "s@/usr/local@$out@" config.mk
+            '';
 
-        makeFlags = [ "CC=${pkgs.stdenv.cc.targetPrefix}cc" ];
-      };
+            makeFlags = [ "CC=${pkgs.stdenv.cc.targetPrefix}cc" ];
+          };
+        in
+        {
+          packages.default = pkg;
+        };
     in
-    {
-      packages.x86_64-linux.default = pkg;
-    };
+    flake-utils.lib.eachDefaultSystem make;
 }
